4eb40f89c37d02935f4f12eb8adcc8107606273f
ClimberUK <jason@pa.tourplan.com>
2017-06-29 09:30:48 +1200

    First commit

---
+++ SchoolsDB/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolsDB
+{
+    public class DAL
+    {
+        public string GetStudentName(int id)
+        {
+            using (var dbCtx = new SchoolDBEntities())
+            {
+                var std = dbCtx.Students.Where(s => s.StudentID == id).FirstOrDefault();
+                return std.StudentName;
+            }
+        }
+    }
+}
